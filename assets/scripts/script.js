79ce368878cd18b7e03245ba70af6a51a7b58f71
Vansh Goyal <Vansh.goyal@edu.sait.ca>
2026-01-16 18:32:47 -0700

    extra spacing which is not needed in script is removed

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,16 +2,16 @@
 
 // sample data - expanded Star Wars characters with varied ages
 const characters = [
-    { id: 1, name: "Luke Skywalker", age: 23 },
-    { id: 2, name: "Darth Vader", age: 45 },
-    { id: 3, name: "Princess Leia", age: 23 },
-    { id: 4, name: "Obi-Wan Kenobi", age: 57 },
-    { id: 5, name: "Yoda", age: 900 },
-    { id: 6, name: "Han Solo", age: 32 },
-    { id: 7, name: "Chewbacca", age: 234 },
-    { id: 8, name: "R2-D2", age: 33 },
-    { id: 9, name: "C-3PO", age: 112 },
-    { id: 10, name: "Padmé Amidala", age: 27 },
+  { id: 1, name: "Luke Skywalker", age: 23 },
+  { id: 2, name: "Darth Vader", age: 45 },
+  { id: 3, name: "Princess Leia", age: 23 },
+  { id: 4, name: "Obi-Wan Kenobi", age: 57 },
+  { id: 5, name: "Yoda", age: 900 },
+  { id: 6, name: "Han Solo", age: 32 },
+  { id: 7, name: "Chewbacca", age: 234 },
+  { id: 8, name: "R2-D2", age: 33 },
+  { id: 9, name: "C-3PO", age: 112 },
+  { id: 10, name: "Padmé Amidala", age: 27 },
 ];
 
 // broken test data for exercise 6
@@ -166,16 +166,16 @@ characterNamePropertyCheck(characters, "error-handling-list", "error-messages", 
 console.log("Result 6:")
 
 const brokenCharacters = [
-    { id: 1, age: 23 },
-    { id: 2, name: "Darth Vader", age: 45 },
-    { id: 3, name: "Princess Leia", age: 23 },
-    { id: 4, age: 57 },
-    { id: 5, name: "Yoda", age: 900 },
-    { id: 6, name: "Han Solo", age: 32 },
-    { id: 7, name: "Chewbacca", age: 234 },
-    { id: 8, age: 33 },
-    { id: 9, name: "C-3PO", age: 112 },
-    { id: 10, age: 27 },
+  { id: 1, age: 23 },
+  { id: 2, name: "Darth Vader", age: 45 },
+  { id: 3, name: "Princess Leia", age: 23 },
+  { id: 4, age: 57 },
+  { id: 5, name: "Yoda", age: 900 },
+  { id: 6, name: "Han Solo", age: 32 },
+  { id: 7, name: "Chewbacca", age: 234 },
+  { id: 8, age: 33 },
+  { id: 9, name: "C-3PO", age: 112 },
+  { id: 10, age: 27 },
 ];
 
 characterNamePropertyCheck(brokenCharacters, "broken-array-list", "broken-array-errors", 1000, "filter-age-result-ex-6");
